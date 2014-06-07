32a4d791e77dae6a8de85f42f11e6228b35987df
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-06-07 21:27:05 +0300

    adding party service

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,36 +1,21 @@
 'use strict';
 
 angular.module('musicPcApp')
-  .controller('MainCtrl', function ($scope, Auth) {
+  .controller('MainCtrl', function ($scope, Auth, Party) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
 
-    $scope.parties = [
-      'party 1',
-      'party 2',
-      'party 3',
-      'party 4',
-      'party 5',
-      'party 6',
-      'party 7',
-      'party 8'
-    ]
-
-    $scope.songs = [
-      'song 1',
-      'song 2',
-      'song 3',
-      'song 4',
-      'song 5',
-      'song 6',
-      'song 7'
-    ];
+    Party.getUserParties().success(function(data) {
+      $scope.parties = data.user.parties;
+    });
 
     $scope.partySelected = function(party) {
-      alert(party);
+      Party.getParty(party.id).success(function(data) {
+        $scope.party = data.party; 
+      });
     };
 
     $scope.songSelected = function(song) {
---
+++ app/scripts/services/party.js
@@ -0,0 +1,16 @@
+'use strickt';
+
+var URL = 'http://music-hasalon-api.herokuapp.com';
+
+angular.module('musicPcApp')
+  .factory('Party', function($http, Auth) {
+    return {
+      getUserParties: function() {
+        var user = Auth.getUser();
+        return $http.get(URL + '/users/1');
+      },
+      getParty: function(partyId) {
+        return $http.get(URL + '/parties/' + partyId);
+      }
+    }
+  });
